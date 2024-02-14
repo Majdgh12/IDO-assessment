4286bc160ba181139320ac7618f3d05500271e60
majd <majd@example.com>
2024-02-14 18:14:41 +0200

    first commit

---
+++ BackEnd/BackEnd/Models/updateTask.cs
@@ -0,0 +1,18 @@
+﻿namespace BackEnd.Models
+{
+   
+    
+        public class updateTask
+        {
+            public int id { get; set; }
+            public string fieldToUpdate { get; set; }
+            public string newContent { get; set; }
+            public updateTask(int id, string fieldToUpdate, string newContent)
+            {
+                this.id = id;
+                this.fieldToUpdate = fieldToUpdate;
+                this.newContent = newContent;
+            }
+        }
+    
+}
